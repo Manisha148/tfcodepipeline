8cc215b51dfaf0074fbadf454e2d0a15422cb794
Manisha148 <70441355+Manisha148@users.noreply.github.com>
2023-05-11 09:51:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_codebuild_project" "my_project" {
   name          = "my-project"
-  service_role  = "arn:aws:iam::124288123671:role/awsrolecodebuild"
+  service_role  = "arn:aws:iam::124288123671:role/awsrolecodebuild\n"
   source {
     type            = "CODEPIPELINE"
     buildspec       = "buildspec.yml"
@@ -14,17 +14,21 @@ resource "aws_codebuild_project" "my_project" {
 }
 
 resource "aws_codepipeline" "my_pipeline" {
-  name     = "my-pipeline"
-  role_arn = "arn:aws:iam::124288123671:role/awsrolecodepipeline"
+  name     = "my_pipeline"
+  role_arn = aws_iam_role.my_role.arn
 
   artifact_store {
-    location = "my-artifact-bucket"
     type     = "S3"
+    location = aws_s3_bucket.my_bucket.bucket
     encryption_key {
-      id   = "my-kms-key"
+      id   = aws_kms_key.my_kms_key.id
       type = "KMS"
     }
   }
+
+  # Rest of the code
+}
+
 
   stage {
     name = "Source"
